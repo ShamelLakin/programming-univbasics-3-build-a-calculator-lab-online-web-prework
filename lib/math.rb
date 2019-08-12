f50aa1e629b49171e93dc6c48509fd58a0fc73d9
Shamel Lakin <christlife4u@gmail.com>
2019-08-12 16:27:26 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+   5 + 4
+   return 9
 end
 
 def subtraction(num1, num2)
-
+  10 - 5
+  return 5
 end
 
 def division(num1, num2)
-
+  50 / 2
+  return 25
 end
 
 def multiplication(num1, num2)
-
+   4 * 30
+   return 120
 end
 
 def modulo(num1, num2)
-
+  34 ** 5
+  return 4
 end
 
 def square_root(num)
-
+ Math.sqrt(81)
+ return 9
 end
