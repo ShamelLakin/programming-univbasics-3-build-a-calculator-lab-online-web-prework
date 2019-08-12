bca0cbc4fc0c1da4cbf58c34598b26363ab3b5e8
Shamel Lakin <christlife4u@gmail.com>
2019-08-12 16:22:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,25 @@
 def addition(num1, num2)
-
+   5 + 5
+   return 10
 end
 
 def subtraction(num1, num2)
-
+  5 - 5
+  return 0
 end
 
 def division(num1, num2)
-
+  10 / 5
 end
 
 def multiplication(num1, num2)
-
+   5 * 5
 end
 
 def modulo(num1, num2)
-
+  3 ** 5
 end
 
 def square_root(num)
-
+ Math.sqrt(5)
 end
